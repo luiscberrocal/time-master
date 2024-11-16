1a326d0b973ff5f7ef70ed191c20e8dfd93f263f
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2024-11-16 07:50:56 -0500

    Linting.

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,9 +5,11 @@ import pytest
 
 @pytest.fixture(scope="session")
 def output_folder() -> Path:
+    """Return the output folder for the tests."""
     return Path(__file__).parent.parent / "output"
 
 
 @pytest.fixture(scope="session")
 def fixtures_folder() -> Path:
+    """Return the fixtures folder for the tests."""
     return Path(__file__).parent / "fixtures"
--- tests/test_time_master.py
+++ tests/test_time_master.py
@@ -1,27 +1,9 @@
 #!/usr/bin/env python
 """Tests for `time_master` package."""
 
-import pytest
 from click.testing import CliRunner
 
 from time_master import cli
-
-
-@pytest.fixture
-def response() -> None:
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
-
-
-def test_content(response) -> None:
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
-    del response
 
 
 def test_command_line_interface() -> None:
