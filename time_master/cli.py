5bda23fd5f79e7d81a0ce992b4fe56b547c2a4df
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2024-11-16 08:29:30 -0500

    Added about

--- time_master/cli.py
+++ time_master/cli.py
@@ -2,14 +2,18 @@
 
 import click
 
+from time_master.commands.core import about
 
-@click.command()
+
+@click.group()
 def main() -> None:
     """Main entrypoint."""
     click.echo("time-master")
     click.echo("=" * len("time-master"))
     click.echo("Time Warrior and Task Warrior simple CLI.")
 
+
+main.add_command(about)
 
 if __name__ == "__main__":
     main()  # pragma: no cover
---
+++ time_master/commands/core.py
@@ -0,0 +1,19 @@
+import sys
+from platform import python_version
+
+import click
+
+from .. import __version__ as current_version
+
+@click.command()
+def about():
+    banner_char = '-'
+    app_name = "Time Master"
+    display_name = f'{app_name} version {current_version}'
+    length = len(display_name) + 4
+    click.echo(banner_char * length)
+    click.echo(f'{banner_char} {display_name} {banner_char}')
+    click.echo(banner_char * length)
+    click.echo(f'Operating System: {sys.platform}')
+    click.echo(f'Python : {python_version()}')
+
